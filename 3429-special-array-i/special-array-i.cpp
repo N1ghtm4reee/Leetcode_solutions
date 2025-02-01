3a24da971ff7f6299ee66a6b155a4c4f69a5202d
! N1ghtm4r3e <ayoube_akhrif2015@outlook.fr>
2025-02-01 01:14:22 +0100

    Time: 0 ms (100.00%) | Memory: 27.6 MB (86.97%) - LeetSync

---
+++ 3429-special-array-i/special-array-i.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isArraySpecial(vector<int>& nums) {
+        int len = nums.size();
+        if (len <= 1)
+            return true;
+        for (int i = 0;i < len -1; i++)
+        {
+            if ((nums[i] % 2) == (nums[i + 1] % 2))
+                return false;
+        }
+        return true;
+    }
+};
